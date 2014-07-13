748d14f79d68116aa87f523c11371c3bfb1918bc
Кондрашов Олег <okondrashov@gmail.com>
2014-07-13 23:17:30 +0400

    No errors now?

--- cmsplugin_simpblock/models.py
+++ cmsplugin_simpblock/models.py
@@ -4,6 +4,6 @@ from django.utils.translation import ugettext_lazy as _
 
 class Simpblock(CMSPlugin):
 	imgsrc = models.ImageField(_("Image"), upload_to='simpblock/')
-	the_title = models.CharField(_("Title"))
-	the_link = models.CharField(_("Link"))
+	the_title = models.CharField(_("Title"), max_length=100)
+	the_link = models.CharField(_("Link"), max_length=100)
 	the_text = models.TextField(_("Text"))
